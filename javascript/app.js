aae7d5db2e8dd23afcd960a7c4fc11ecf074719d
sina koushki <sinaderik7@gmail.com>
2025-02-08 16:10:15 +0330

    class is defined

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,5 @@
+// constructor function 
+// In the code below the showPersonInfo function will be created for every instance of the object.
 function Person(name,age){
     this.name = name;
     this.age = age;
@@ -7,5 +9,39 @@ function Person(name,age){
     }
 }
 
+
+// Here we create a prototype function(showCarInfo) for the Person object.
+// This way the function will be created only once and shared between all the instances of the object.
+function Car(brand, year){
+    this.brand = brand;
+    this.year = year;    
+}
+
+Car.prototype.showCarInfo = function(){
+    console.log(`This is a ${this.year} ${this.brand} ${this.model}`);
+}
+
 const person1 = new Person('John', 25);
-person1.showPersonInfo();
+const person2 = new Person('Sara', 30);
+
+const car1= new Car('Toyota', 2019);
+const car2= new Car('Honda', 2018);
+
+
+// class is syntactic sugar for constructor function
+class Laptop{
+    constructor(brand, price){
+        this.brand = brand;
+        this.price = price;
+    }
+
+    showLaptopInfo(){
+        console.log(`This is a ${this.brand} laptop and worth ${this.price}`);
+    }
+}
+
+const laptop1 = new Laptop('Dell', 1000);
+const laptop2 = new Laptop('HP', 800);
+
+console.log(laptop1)
+console.log(laptop2)
