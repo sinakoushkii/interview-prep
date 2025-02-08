4210d867c62f0394ce1f57931a0697972c6d6775
sina koushki <sinaderik7@gmail.com>
2025-02-08 22:39:04 +0330

    rest parameter and spread operator added

--- javascript/app.js
+++ javascript/app.js
@@ -1,47 +1,75 @@
-// constructor function 
+// constructor function
 // In the code below the showPersonInfo function will be created for every instance of the object.
-function Person(name,age){
-    this.name = name;
-    this.age = age;
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 
-    this.showPersonInfo = function(){
-        console.log(`This is ${this.name} and he is ${this.age} years old`);
-    }
+  this.showPersonInfo = function () {
+    console.log(`This is ${this.name} and he is ${this.age} years old`);
+  };
 }
-
 
 // Here we create a prototype function(showCarInfo) for the Person object.
 // This way the function will be created only once and shared between all the instances of the object.
-function Car(brand, year){
-    this.brand = brand;
-    this.year = year;    
+// This is called Prototype Design Pattern
+function Car(brand, year) {
+  this.brand = brand;
+  this.year = year;
 }
 
-Car.prototype.showCarInfo = function(){
-    console.log(`This is a ${this.year} ${this.brand} ${this.model}`);
-}
+Car.prototype.showCarInfo = function () {
+  console.log(`This is a ${this.year} ${this.brand} ${this.model}`);
+};
 
-const person1 = new Person('John', 25);
-const person2 = new Person('Sara', 30);
+const person1 = new Person("John", 25);
+const person2 = new Person("Sara", 30);
 
-const car1= new Car('Toyota', 2019);
-const car2= new Car('Honda', 2018);
-
+const car1 = new Car("Toyota", 2019);
+const car2 = new Car("Honda", 2018);
 
 // class is syntactic sugar for constructor function
-class Laptop{
-    constructor(brand, price){
-        this.brand = brand;
-        this.price = price;
-    }
+class Laptop {
+  constructor(brand, price) {
+    this.brand = brand;
+    this.price = price;
+  }
 
-    showLaptopInfo(){
-        console.log(`This is a ${this.brand} laptop and worth ${this.price}`);
-    }
+  showLaptopInfo() {
+    console.log(`This is a ${this.brand} laptop and worth ${this.price}`);
+  }
 }
 
-const laptop1 = new Laptop('Dell', 1000);
-const laptop2 = new Laptop('HP', 800);
+const laptop1 = new Laptop("Dell", 1000);
+const laptop2 = new Laptop("HP", 800);
 
-console.log(laptop1)
-console.log(laptop2)
+console.log(laptop1);
+console.log(laptop2);
+
+const getName = () => {
+  console.log("Hello World");
+};
+
+// rest parameter
+// used to represent an indefinite number of arguments as an array and when function declaration
+const sum = (...args) => {
+  let total = 0;
+  args.forEach((arg) => {
+    total += arg;
+  });
+  return total;
+};
+
+const result = sum(10, 30, 100, 20);
+
+// spread operator
+// used to split up array elements or object properties and when function call
+const codes = [101, 202, 303, 404];
+newcodes = [...codes, 505, 606, 707];
+
+let fourNumbers = [5, 6, 7, 8];
+
+function addFourNumbers(num1, num2, num3, num4) {
+  return num1 + num2 + num3 + num4;
+}
+
+const finallResult=addFourNumbers(...fourNumbers);
